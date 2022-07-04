31aea213e4e0eca27130ad07a1d024c23de80cd3
郑伟鹏 <610299213@qq.com>
2022-07-04 21:31:49 +0800

    单例设计模式-饿汉&懒汉

--- model/src/Singleton.java
+++ model/src/Singleton.java
@@ -6,12 +6,18 @@
  */
 public class Singleton {
 
+
     /**
-     * 这是一个单例模式
+     * 单例设计模式之饿汉式,有需要才创建
+     * 这个使用的双重校验锁 所以是线程安全的
+     */
+
+    /**
+     * volatile 防止指令重排序
      */
     private volatile static Singleton singletonModel = null;
 
-    private static Singleton getSingletonModel() {
+    public static Singleton getSingletonModel() {
 
         if ( null == singletonModel){
             synchronized (Singleton.class){
@@ -23,10 +29,21 @@ public class Singleton {
         return singletonModel;
     }
 
+    /**
+     * 饿汉式,使用前就创建好。
+     * 线程安全,但是浪费内存
+     */
+    // private static Singleton singletonModel1 = new Singleton();
+    //
+    // public static Singleton getInstance(){
+    //     return singletonModel1;
+    // }
+
+
     public static void main(String[] args) {
 
-        Singleton singletonModel = Singleton.getSingletonModel();
-        System.out.println(singletonModel);
+        System.out.println("懒汉式 双重校验锁: " + Singleton.getSingletonModel());
+        // System.out.println("饿汉式 使用前就创建好: " + Singleton.getInstance());
     }
 
 }
